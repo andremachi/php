4a1a71ffe45fd07637f0b12fd271cf8bda6f878e
andremachi <35235463+andremachi@users.noreply.github.com>
2018-01-10 10:17:47 -0200

    resolvendo conflito de novo, nao mudou no github

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 
 </php
 
-<<<<<<< HEAD
    echo "Olá Mundo!!!!!?456";
 
    echo "Olá Udemy!"
-=======
-   echo "Olá Mundo!!!!!?123";
 
-   echo "Olá hcodeeu!!;
->>>>>>> aba441b8db002f5e4599670a6113f44a1077c1d8
 
 >?
